df09100a5ca4f911e181563db4252c6d49dd2c57
Alejandro Esaú Alvarez <alvalex_@hotmail.com>
2017-05-14 20:29:47 -0500

    Version 2 Proyecto Tiny Travel
    Se actualizaron los avances por medio de upload

--- test/core/src/com/tinytravel/game/TinyTravel.java
+++ test/core/src/com/tinytravel/game/TinyTravel.java
@@ -2,52 +2,60 @@ package com.tinytravel.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public class TinyTravel extends ApplicationAdapter {
+import com.badlogic.gdx.ApplicationListener;
+public class TinyTravel extends ApplicationAdapter implements ApplicationListener{
 	SpriteBatch batch;
-	Texture img;
-	private OrthographicCamera camera;
-	private Insecto insect1;
-	private Insecto insect2;
+	private Celula celula;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-
-		img = new Texture("badlogic.jpg");
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false,800,480);
-		camera.update();
-
-		insect1 = new Insecto(100,100);
-		insect2 = new Insecto(120,100);
-
+		celula = new Celula(400,420);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//insect1.setX(insect1.getX()+1);
+		int w = Gdx.graphics.getWidth(); //Obtener las dimensiones del graphico en x
+		int h = Gdx.graphics.getHeight(); //Obtener las dimensiones del graphico en y
 
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-
+	    if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+	    	celula.setX(celula.getX()-10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+	    	celula.setX(celula.getX()+10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+			celula.setY(celula.getY()-10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+			celula.setY(celula.getY()+10);
+		}
+		//Restriccion
+		if(celula.getX()>=w-60){
+			celula.setX(celula.getX()-10);
+		}
+		if(celula.getX()<=0){
+			celula.setX(celula.getX()+10);
+		}
+		if(celula.getY()>=h-60){
+			celula.setY(celula.getY()-10);
+		}
+		if(celula.getY()<=0){
+			celula.setY(celula.getY()+10);
+		}
 		batch.begin();
-		insect1.render(batch);
-		//insect2.render(batch);
-
-		//batch.draw(img, 0, 0);
+		celula.render(batch);
 		batch.end();
 	}
-	
-	/*@Override
+	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
-	}*/
+	}
 }
